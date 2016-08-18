d115fb3bf0c83b7cb6d1589f2a7dbd82d63c116c
David Symons <Dwsymons@optusnet.com.au>
2016-08-18 21:39:46 +1000

    Add docker-machine booting to docker-machine-init

--- docker-machine-init.sh
+++ docker-machine-init.sh
@@ -1,2 +1,5 @@
+docker-machine start
+eval $(docker-machine env)
+
 docker-machine ssh default 'sudo mkdir --parents /e/Documents/development'
 docker-machine ssh default 'sudo mount -t vboxsf e/Documents/development /e/Documents/development'
