32c54b89524c5be5efb1d260106b280693c93397
David Symons <Dwsymons@optusnet.com.au>
2016-08-25 18:13:02 +1000

    Add better logging to docker-machine-init

--- docker-machine-init.sh
+++ docker-machine-init.sh
@@ -1,5 +1,13 @@
-docker-machine start
-eval $(docker-machine env)
+#!/bin/bash -e
 
-docker-machine ssh default 'sudo mkdir --parents /e/Documents/development'
-docker-machine ssh default 'sudo mount -t vboxsf e/Documents/development /e/Documents/development'
+indent() { sed 's/^/  /'; }
+
+echo 'Launching docker machine...'
+docker-machine start | indent
+
+echo 'Configuring environment...'
+eval $(docker-machine env) | indent
+
+echo 'Mapping local folders...'
+docker-machine ssh default 'sudo mkdir --parents /e/Documents/development' | indent
+docker-machine ssh default 'sudo mount -t vboxsf e/Documents/development /e/Documents/development' | indent
