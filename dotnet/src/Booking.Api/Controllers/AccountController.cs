ff2881914dda65562e34ea8c5fee99d13743dabf
David Symons <Dwsymons@optusnet.com.au>
2016-09-02 18:51:40 +1000

    Fix issue with incorrect generic type in AccountController

--- dotnet/src/Booking.Api/Controllers/AccountController.cs
+++ dotnet/src/Booking.Api/Controllers/AccountController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Booking.Api.General;
 using Booking.Api.Logging;
@@ -17,71 +18,71 @@ namespace Booking.Api.Controllers
     {
         private ErrorResponseFactory ErrorResponseFactory { get; } = null;
         private AccountOptions Options { get; } = null;
-        
+
         private ILogger<AccountController> Logger { get; } = null;
         private IStringLocalizer Localizer { get; } = null;
-        
-        private SignInManager<IdentityUser> SignInManager { get; } = null;
-        private UserManager<IdentityUser> UserManager { get; } = null;
-        
+
+        private SignInManager<IdentityUser<Guid>> SignInManager { get; } = null;
+        private UserManager<IdentityUser<Guid>> UserManager { get; } = null;
+
         public AccountController(
             ErrorResponseFactory errorResponseFactory,
             IOptions<AccountOptions> optionsAccessor,
             ILogger<AccountController> logger,
             IStringLocalizer<AccountController> localizer,
-            SignInManager<IdentityUser> signInManager,
-            UserManager<IdentityUser> userManager)
+            SignInManager<IdentityUser<Guid>> signInManager,
+            UserManager<IdentityUser<Guid>> userManager)
         {
             this.ErrorResponseFactory = errorResponseFactory;
             this.Options = optionsAccessor.Value;
-            
+
             this.Logger = logger;
             this.Localizer = localizer;
-            
+
             this.SignInManager = signInManager;
             this.UserManager = userManager;
         }
-        
+
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody]LoginModel model)
-        {            
-            if(!ModelState.IsValid || model == null)
+        {
+            if (!ModelState.IsValid || model == null)
                 return ErrorResponseFactory.InvalidModelState(ModelState);
-            
+
             model.Normalize();
-            
+
             var validationResult = await (new LoginModelValidator()).ValidateAsync(model);
-            if(!validationResult.IsValid)
+            if (!validationResult.IsValid)
                 return ErrorResponseFactory.ValidationFailed(validationResult.Errors);
-            
+
             Logger.LoginAttempted(model.Email);
-            
+
             var user = await UserManager.FindByEmailAsync(model.Email);
-            if(user == null)
+            if (user == null)
             {
                 var error = ErrorResponseFactory.GenerateModel(ErrorCode.FailedLogin);
                 return new ObjectResult(error) { StatusCode = 401 };
             }
-            
+
             var signInResult = await SignInManager.PasswordSignInAsync(
                 user, model.Password, model.RememberMe.Value,
                 Options.LockoutOnFailedLogin
             );
-            
-            if(!signInResult.Succeeded)
+
+            if (!signInResult.Succeeded)
             {
                 ErrorModel error;
                 if (signInResult.RequiresTwoFactor)
                     error = ErrorResponseFactory.GenerateModel(ErrorCode.TwoFactorRequired);
                 else
                     error = ErrorResponseFactory.GenerateModel(ErrorCode.FailedLogin);
-                    
+
                 return new ObjectResult(error) { StatusCode = 401 };
             }
-            
+
             return new StatusCodeResult(200);
         }
-        
+
         [HttpPost("logout")]
         public async Task<IActionResult> Logout()
         {
