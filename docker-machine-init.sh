8dbc2169551a17249ecf2bd49aa2128aafbd60e9
David Symons <Dwsymons@optusnet.com.au>
2016-09-02 18:50:11 +1000

    Remove environment config from docker-machine-init

--- docker-machine-init.sh
+++ docker-machine-init.sh
@@ -5,9 +5,6 @@ indent() { sed 's/^/  /'; }
 echo 'Launching docker machine...'
 docker-machine start | indent
 
-echo 'Configuring environment...'
-eval $(docker-machine env) | indent
-
 echo 'Mapping local folders...'
 docker-machine ssh default 'sudo mkdir --parents /e/Documents/development' | indent
 docker-machine ssh default 'sudo mount -t vboxsf e/Documents/development /e/Documents/development' | indent
