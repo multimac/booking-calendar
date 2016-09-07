1799f37eb16fb5eefcfb0801694a7d922bbe1e75
David Symons <Dwsymons@optusnet.com.au>
2016-09-07 21:29:40 +1000

    Tweak normalization in ErrorModel

--- dotnet/src/Booking.Api/Models/ErrorModel.cs
+++ dotnet/src/Booking.Api/Models/ErrorModel.cs
@@ -7,35 +7,35 @@ namespace Booking.Api.Models
         /* 0 - 29: System errors */
         Unknown = 0,
         InternalServerError,
-        
+
         /* 30 - 49: Request errors */
         ParseError = 30,
         ValidationError,
-        
+
         /* 50 - 69: Login errors */
         FailedLogin = 50,
         TwoFactorRequired,
     }
-    
+
     public class ErrorModel : IModel
     {
         public ErrorCode? Code { get; set; } = ErrorCode.Unknown;
         public string Reason { get; set; } = null;
-        
+
         public string Message { get; set; } = null;
-        
+
         public ErrorModel[] InnerErrors { get; set; } = null;
-        
+
         public ErrorModel() { }
-        
+
         public void Normalize()
         {
             Code = Code ?? ErrorCode.Unknown;
             Reason = Reason.NullIfWhiteSpace();
-            
+
             Message = Message.NullIfWhiteSpace();
-            
-            InnerErrors = (InnerErrors.Length == 0 ? null : InnerErrors);
+
+            InnerErrors = InnerErrors ?? new ErrorModel[] { };
         }
     }
 }
