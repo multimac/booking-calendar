185c22a3b35b885382dacb446fa1948a42be6ea1
David Symons <Dwsymons@optusnet.com.au>
2016-09-10 23:36:24 +1000

    Set up JSPM, typings and inital work on aurelia

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -7,19 +7,24 @@ var del = require("del");
 gulp.task("default", ["prod"]);
 gulp.task("prod", ["typescript"]);
 
-gulp.task("clean", function() {
-    return del(["static/**/*.js"]);
+gulp.task("clean", function () {
+    return del([
+        "node_modules", "typings",
+            
+        "static/**", "!static",
+        "!static/system-config.js"
+    ]);
 });
 
 
 var tsProject = ts.createProject("tsconfig.json", {
-    typescript: require("typescript")
+    typescript: require("typescript"), rootDir: "."
 });
-gulp.task("typescript", function() {
+gulp.task("typescript", function () {
     var typescript = tsProject.src()
         .pipe(sourcemaps.init())
         .pipe(ts(tsProject));
-    
+
     return typescript.js
         .pipe(sourcemaps.write())
         .pipe(gulp.dest("./static/"));
