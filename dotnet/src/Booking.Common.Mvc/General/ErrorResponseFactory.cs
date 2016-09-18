377bc92aa26b2ae529ae3561657993482ac1203d
David Symons <Dwsymons@optusnet.com.au>
2016-09-18 15:29:06 +1000

    Moved InvalidModelState and ValidationFailed responses to Booking.Common.Mvc

--- dotnet/src/Booking.Common.Mvc/General/ErrorResponseFactory.cs
+++ dotnet/src/Booking.Common.Mvc/General/ErrorResponseFactory.cs
@@ -58,5 +58,33 @@ namespace Booking.Common.Mvc.General
             
             return new ObjectResult(model) { StatusCode = 500 };
         }
+        /// <summary>
+        /// Generates a response with a message for each error in the <see cref="ModelStateDictionary"/>
+        /// </summary>
+        /// <param name="state">The state containing all error messages</param>
+        public static IActionResult InvalidModelState(this ErrorResponseFactory factory, Microsoft.AspNetCore.Mvc.ModelBinding.ModelStateDictionary state)
+        {
+            var model = factory.GenerateModel(ErrorCode.ParseError,
+                state.Where(kv => kv.Value.Errors.Count > 0)
+                    .SelectMany(kv => kv.Value.Errors)
+                    .Select(err => factory.GenerateModel(ErrorCode.ParseError, err.ToString()))
+                    .ToArray()
+            );
+            
+            return new ObjectResult(model) { StatusCode = 400 };
+        }
+        /// <summary>
+        /// Generates a response with a message for each validation error in the <see cref="IList<ValidationFailure>"/>
+        /// </summary>
+        /// <param name="errors">The list of validation errors that occurred</param>
+        public static IActionResult ValidationFailed(this ErrorResponseFactory factory, IList<FluentValidation.Results.ValidationFailure> errors)
+        {
+            var model = factory.GenerateModel(ErrorCode.ValidationError,
+                errors.Select(err => factory.GenerateModel(ErrorCode.ValidationError, err.ToString()))
+                    .ToArray()
+            );
+
+            return new ObjectResult(model) { StatusCode = 400 };
+        }
     }
 }
