cf5df9a5283f3933c1acde6f5ef1982d353fa26c
David Symons <Dwsymons@optusnet.com.au>
2016-09-25 18:14:58 +1000

    Correct capitalization

--- dotnet/src/Booking.Common/Extensions/StringExtensions.cs
+++ dotnet/src/Booking.Common/Extensions/StringExtensions.cs
@@ -1,16 +1,14 @@
-using System;
-
 namespace Booking.Common.Extensions
 {
     public static class StringExtensions
     {
         public static string NullIfEmpty(this string s)
         {
-            return (String.IsNullOrEmpty(s) ? null : s);
+            return (string.IsNullOrEmpty(s) ? null : s);
         }
         public static string NullIfWhiteSpace(this string s)
         {
-            return (String.IsNullOrWhiteSpace(s) ? null : s);
+            return (string.IsNullOrWhiteSpace(s) ? null : s);
         }
     }
 }
